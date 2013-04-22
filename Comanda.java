f4fdceaef08bedcf5339102942dd974e5cc238bf
Ruben Macias <ruben.macias@insbaixcamp.cat>
2013-04-22 17:16:17 +0200

    Add base files

---
+++ Comanda.java
@@ -0,0 +1,12 @@
+
+public class Comanda {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Proveidor.java
@@ -0,0 +1,12 @@
+
+public class Proveidor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
